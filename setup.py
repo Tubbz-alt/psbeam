581d792923facd6f56112b17789c8e195e97211d
apraSLAC <dd.apra93@gmail.com>
2017-03-30 12:00:18 -0700

    Filled in setup and added a license

--- setup.py
+++ setup.py
@@ -1,20 +1,9 @@
+from setuptools import (setup, find_packages)
 
-try:
-    from setuptools import setup
-except ImportError:
-    from distutils.core import setup
-
-config = {
-    'description':       '',
-    'author':            'apra (Abdullah P Rashed Ahmed)',
-    'url':               '',
-    'download_url':      '',
-    'author_email':      'apra@slac.stanford.edu',
-    'version':           '0.1',
-    'install_requires':  ['nose'],
-    'packages':          ['beamDetector'],
-    'scripts':           [],
-    'name':              'psbeam'
-}
-
-setup(**config)
+setup(name        = 'psbeam',
+      description = 'Image Processing Pipelines for Photon Controls and Data Systems',
+      version     = '0.0.1',
+      license     = 'BSD',
+      author      = 'SLAC National Accelerator Laboratory',
+      packages    = find_packages(),
+      )
