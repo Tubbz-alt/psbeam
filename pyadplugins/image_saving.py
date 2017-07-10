b8951373fad1a6084e2beb208d7bc42f7b22004e
apra93 <dd.apra93@gmail.com>
2017-07-10 16:46:51 -0700

    Changed images to template_images and corrected the one reference to it. Also added an underscore to image_saving.

--- pyadplugins/image_saving.py
+++ pyadplugins/image_saving.py
@@ -24,14 +24,14 @@ import psbeam.preprocessing as prep
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.DEBUG)
 
-dir_path = "/reg/g/pcds/pyps/apps/skywalker/images/"
+dir_path = "/reg/g/pcds/pyps/apps/skywalker/template_images/"
 
 def save(array, height=None, width=None):
     image = np.reshape(array, (height, width))
     i = 0
     img_path = Path("{0}image_{1}.png".format(dir_path, i))
-    
-    while img_path.exists():
-        i += 1
-    cv2
-    return 0
+    pass
+    # while img_path.exists():
+    #     i += 1
+    # cv2
+    # return 0
