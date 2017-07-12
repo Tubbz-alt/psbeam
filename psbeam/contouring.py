621501f4af535e66fe53a7f86b58b0e54680ff1e
apra93 <dd.apra93@gmail.com>
2017-07-11 18:11:10 -0700

    Made contouring doc strings sphinx compliant.

--- psbeam/contouring.py
+++ psbeam/contouring.py
@@ -24,29 +24,49 @@ from .template_images import circle_small
 from .beamexceptions import NoContoursPresent
 
 logger = logging.getLogger(__name__)
-circle_small_contour = get_largest_contour(circle_small, factor=0)
 
 def get_contours(image, factor=3):
     """
     Returns the contours of an image according to a mean-threshold.
+
+    Parameters
+    ----------
+    image : np.ndarray
+    	Image to extract the contours from.
+
+    factor : int, optional
+    	Number of times to multiply the std by before adding to the mean for
+    	thresholding.
     """
     _, image_thresh = cv2.threshold(
         image, image.mean() + image.std()*factor, 255, cv2.THRESH_TOZERO)
     _, contours, _ = cv2.findContours(image_thresh, 1, 2)
     return contours
 
-def get_largest_contour(image, contours=None, factor=3):
+def get_largest_contour(image=None, contours=None, factor=3):
     """
-    Returns largest contour of the contour list.
+    Returns largest contour of the contour list. Either an image or a contour
+    must be passed.
 
-    Args:
-        image (np.ndarray): Image to extract the contours from.
-    Returns:
-        np.ndarray. First element of contours list which lists the boundaries 
-            of the contour.
+    Function is making an implicit assumption that there will only be one
+    (large) contour in the image. 
 
-    Method is making an implicit assumption that there will only be one
-    contour (beam) in the image. 
+    Parameters
+    ----------
+    image : np.ndarray, optional
+    	Image to extract the contours from.
+
+    contours : np.ndarray, optional
+    	Contours found on an image.
+
+    factor : int, optional
+    	Number of times to multiply the std by before adding to the mean for
+    	thresholding.    
+
+    Returns
+    -------
+    np.ndarray
+    	Contour that encloses the largest area.
     """
     # Check if contours were inputted
     if contours is None:
@@ -63,15 +83,22 @@ def get_moments(image=None, contour=None):
     """
     Returns the moments of an image.
 
-    Kwargs:
-        image (np.ndarray): Image to calculate moments from.
-        contour (np.ndarray): Beam contour boundaries.
-    Returns:
-        list. List of zero, first and second image moments for x and y.
-
     Attempts to find the moments using an inputted contours first, but if it
     isn't inputted it will compute the contours of the image then compute
     the moments.
+
+    Parameters
+    ----------
+    image : np.ndarray
+    	Image to calculate moments from.
+
+    contour : np.ndarray
+    	Beam contour.
+
+    Returns
+    -------
+    list
+    	List of zero, first and second image moments for x and y.
     """
     try:
         return cv2.moments(contour)
@@ -86,22 +113,35 @@ def get_centroid(M):
     Centroid is computed as being the first moment in x and y divided by the
     zeroth moment.
 
-    Args:
-        M (list): Moments of an image.
-    Returns:
-        tuple. Centroid of the image.
+    Parameters
+    ----------
+    M : list
+    	Moments of an image
+    
+    Returns
+    -------
+    tuple
+    	Centroid of the image
     """    
     return int(M['m10']/M['m00']), int(M['m01']/M['m00'])
 
 def get_bounding_box(image=None, contour=None):
     """
-    Finds the up-right bounding box that contains the inputted contour.
+    Finds the up-right bounding box that contains the inputted contour. Either
+    an image or contours have to be passed.
 
-    Kwargs:
-        image (np.ndarray): Image to get a bounding box for.
-        contour (np.ndarray): Beam contour boundaries.
-    Returns:
-        tuple. Contains x, y, width, height of bounding box.
+    Parameters
+    ----------
+    image : np.ndarray, optional
+    	Image to get a bounding box for.
+
+    contour : np.ndarray, optional
+    	Beam contour.
+
+    Returns
+    -------
+    tuple
+    	Contains x, y, width, height of bounding box.
 
     It should be noted that the x and y coordinates are for the bottom left
     corner of the bounding box. Use matplotlib.patches.Rectangle to plot.
@@ -112,9 +152,28 @@ def get_bounding_box(image=None, contour=None):
         contour, _ = get_largest_contour(image)
         return cv2.boundingRect(contour)
 
+# Define circle_small_contour as a global
+circle_small_contour, _ = get_largest_contour(circle_small, factor=0)
+    
 def get_circularity(contour, method=1):
     """
-    Returns a score of how circular a contour is by comparing it to the template
-    image, "circle_small.png." in the template_images directory.
+    Returns a score of how circular a contour is by comparing it to the
+    contour of the template image, "circle_small.png." in the template_images
+    directory.
+
+    Parameters
+    ----------
+    contour : np.ndarray
+    	Contour to be compared with the circle contour
+
+    method : int, optional
+    	Matches the contours according to an enumeration from 0 to 2. To see
+    	the methods in detail, go to:
+    	http://docs.opencv.org/3.1.0/df/d4e/group__imgproc__c.html#gacd971ae682604ff73cdb88645725968d
+
+    Returns
+    -------
+    float
+    	Value ranging from 0.0 to 1.0 where 0.0 is perfectly similar to a circle
     """
     return cv2.matchshapes(circle_small_contour, contour, method, 0)
