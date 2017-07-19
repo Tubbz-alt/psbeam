aa4c5226421d079ef0154e129ece5d0f50b73f86
apra93 <dd.apra93@gmail.com>
2017-07-19 16:46:03 -0700

    TST: Commented out the tests since they arent implemented.

--- tests/test_plans.py
+++ tests/test_plans.py
@@ -15,26 +15,26 @@ from bluesky.plans import run_wrapper
 # Module #
 ##########
 from .utils import collector
-from psbeam.plans import measure_beam_quality
+# from psbeam.plans import measure_beam_quality
 
-logger = logging.getLogger(__name__)
+# logger = logging.getLogger(__name__)
 
-def test_measure_beam_quality(RE):
-    # import ipdb; ipdb.set_trace()
-    # import IPython; IPython.embed()
-    p = pim.PIM("TEST")
-    test_image = cv2.imread("tests/test_yag_images_01/image97.jpg", 0)
-    p.detector.image1.image = test_image
+# def test_measure_beam_quality(RE):
+#     # import ipdb; ipdb.set_trace()
+#     # import IPython; IPython.embed()
+#     p = pim.PIM("TEST")
+#     test_image = cv2.imread("tests/test_yag_images_01/image97.jpg", 0)
+#     p.detector.image1.image = test_image
     
-    res = {}
-    col = collector("Sim Detector", res)
+#     res = {}
+#     col = collector("Sim Detector", res)
     
     
-    RE(run_wrapper(measure_beam_quality(p.detector, p.detector.image1, 
-                                        read_rate=100, num_reads=10)),
-       subs={'event':[col]})
+#     RE(run_wrapper(measure_beam_quality(p.detector, p.detector.image1, 
+#                                         read_rate=100, num_reads=10)),
+#        subs={'event':[col]})
 
-    import IPython; IPython.embed()
+#     import IPython; IPython.embed()
 
 
                    
