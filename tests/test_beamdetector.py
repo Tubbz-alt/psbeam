0d47fddcffad92c84b4f38bc7eb8c2548af1b920
apra93 <dd.apra93@gmail.com>
2017-07-24 21:47:52 -0700

    TST: Added tests for beamdetector.

--- tests/test_beamdetector.py
+++ tests/test_beamdetector.py
@@ -1,53 +1,34 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+Tests for functions in the beamdetector module
+"""
+############
+# Standard #
+############
+import logging
+
+###############
+# Third Party #
+###############
+import cv2
+import pytest
 import numpy as np
-import beamDetector.detector as bd
-from nose.tools import *
-from utils.cvUtils import  get_images_from_dir
 
-# test_images_01_dir = "tests/test_yag_images_01/"
-# test_images_01 = get_images_from_dir(test_images_01_dir)
-# image = test_images_01[0]
-# det = bd.Detector()
+##########
+# Module #
+##########
+from psbeam.images import testing
+from psbeam.images.templates import circle
+from psbeam.beamexceptions import NoBeamDetected
+from psbeam.beamdetector import detect
 
-# class TestDetector(object):
-#     @classmethod
-#     def setup_class(self):
-#         self.test_images_01_dir = "tests/test_yag_images_01/"
-#         self.test_images_01 = get_images_from_dir(self.test_images_01_dir)
-#         self.image = self.test_images_01[0]
-#         self.det = bd.Detector()
+# detect
 
-#     def test_preprocess_image_type(self):
-#         preprocessed_image = self.det.preprocess(self.image)
-#         assert_equals(preprocessed_image.dtype, np.uint8)
+def test_detect_raises_nobeamdetected_when_no_beam():
+    with pytest.raises(NoBeamDetected):
+        detect(np.zeros((50,50)))
 
-#     def test_get_contours(self):
-#         preprocessed_image = self.det.preprocess(self.image)
-#         contour = self.det.get_contour(preprocessed_image)
-#         assert isinstance(contour, np.ndarray)
-#         assert_equals(contour.shape[1:], (1,2))
-    
-#     def test_get_moments(self):
-#         preprocessed_image = self.det.preprocess(self.image)
-#         contour = self.det.get_contour(preprocessed_image)
-#         M = self.det.get_moments(contour=contour)
-#         assert isinstance(M, dict)
-#         assert_equals(len(M), 24)
-
-#     def test_beam_is_present_type(self):
-#         preprocessed_image = self.det.preprocess(self.image)
-#         contour = self.det.get_contour(preprocessed_image)
-#         M = self.det.get_moments(contour=contour)
-#         assert isinstance(self.det.beam_is_present(M), bool)
-
-#     def test_centroid(self):
-#         preprocessed_image = self.det.preprocess(self.image)
-#         contour = self.det.get_contour(preprocessed_image)
-#         M = self.det.get_moments(contour=contour)
-#         centroid = self.det.get_centroid(M)
-#         assert isinstance(centroid, tuple)
-#         assert isinstance(centroid[0], int)
-#         assert_equals(len(centroid), 2)
-
-    
-
-
+def test_detect_finds_center_of_circle():
+    centroid, bounding_box = detect(circle)
+    assert(centroid == [s/2-1 for s in circle.shape])
