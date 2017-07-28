03deba80acbcad7e2f4bd74891ae10caa4ec720d
apra93 <dd.apra93@gmail.com>
2017-07-28 10:59:07 -0700

    TST: Added SimDetector initializer to conftest and added a quick test for it in prealign_stats.

--- tests/conftest.py
+++ tests/conftest.py
@@ -8,7 +8,36 @@ import logging
 # Third Party #
 ###############
 import pytest
-from pcdsdevices.sim.pim import PIM
+import numpy as np
+from pcdsdevices.sim.areadetector.detectors import SimDetector
+
+##########
+# Module #
+##########
+from psbeam.images.testing import (beam_image_01, beam_image_02, beam_image_03,
+                                   beam_image_04)
+
+beam_images = [beam_image_01, beam_image_02, beam_image_03, beam_image_04]
+
+def return_beam_image(detector):
+    np.random.seed()
+    idx = np.random.randint(0, len(beam_images))
+    detector._n_image = idx        
+    if idx == 3:
+        detector._has_beam = False
+    else:
+        detector._has_beam = True
+    return beam_images[idx].astype(np.uint8)
+
+@pytest.fixture(scope='function')
+def sim_det():
+    det = SimDetector("PSB:SIM")
+    # Initialize has_beam and n_image attribute
+    det._n_image = -1
+    det._has_beam = False
+    # Spoof image2
+    det.image._image = lambda : return_beam_image(det)
+    return det
 
 @pytest.fixture(scope='function')
 def RE():
@@ -17,8 +46,3 @@ def RE():
     """
     RE = RunEngine({})
     return RE
-
-@pytest.fixture(scope='function')
-def fake_pim():
-    p = PIM("TEST")
-    
--- tests/test_prealign_stats.py
+++ tests/test_prealign_stats.py
@@ -18,3 +18,20 @@ import numpy as np
 ##########
 # Module #
 ##########
+from psbeam.beamdetector import detect
+from psbeam.beamexceptions import NoBeamDetected
+from psbeam.filters import contour_area_filter 
+
+def test_sim_det_device_interfaces_properly(sim_det):
+    det = sim_det
+    im_filter = lambda image : contour_area_filter(image, uint_mode="clip")
+    for i in range(10):
+        try:
+            image = det.image.image
+            idx = det._n_image
+            cent, bbox = detect(image, uint_mode="clip", filters=im_filter)
+            assert(det._has_beam)
+        except NoBeamDetected:
+            assert(not det._has_beam)
+        
+    
