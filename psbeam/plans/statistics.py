2a82f7fb11fdb0fbebb46cfa532276021bc83762
apra93 <dd.apra93@gmail.com>
2017-07-31 14:43:16 -0700

    ENH: Added basic statistics plan.

--- psbeam/plans/statistics.py
+++ psbeam/plans/statistics.py
@@ -24,14 +24,14 @@ from pswalker.plans import measure
 # Module #
 ##########
 from ..filters import contour_area_filter
+from ..beamexceptions import NoContoursDetected
+from ..preprocessing import uint_resize_gauss
+from ..contouring import (get_largest_contour, get_moments, get_centroid,
+                          get_contour_size, get_similarity)
 
 logger = logging.getLogger(__name__)
 
 
-# def process_det_data(data, detector):
-
-#     for d in data[detector.name]:
-#     pass
 
 # def process_all_data(data, detectors, n_pool=None):
 #     """
@@ -76,37 +76,109 @@ logger = logging.getLogger(__name__)
 #     return np.reshape(np.array(array), (detector.cam.size.size_y.value,
 #                                         detector.cam.size.size_x.value))
 
-# def characterize(detectors, image_field, num=10, filters=None, delay=None,
-#                  drop_missing=True, kernel=(9,9), resize=1.0, uint_mode="scale",
-#                  min_area=100, factor=3, n_pool=None, **kwargs):
-#     """
-#     Returns a dictionary containing all the relevant statistics of the beam.
-#     """
-#     # Stash the read_attrs of the detectors
-#     read_attr_orig = {det.name : det.read_attrs for det in detectors}
-#     # Replace the read_attrs with the image attr
-#     for det in detectors:
-#         det.read_attrs = [image_field]
+def process_image(image, resize=1.0, kernel=(13,13), uint_mode="scale",
+                  thresh_mode="otsu", thresh_factor=3):
+    """
+    Processes the input image and returns an array of numbers charcterizing the
+    beam.
 
-#     # Apply the default filter
-#     if filters is None:
-#         filters = {image_field : lambda image : contour_area_filter(
-#             to_image(image), kernel=kernel, resize=resize, uint_mode=uint_mode,
-#             min_area=min_area, factor=factor, **kwargs)}
+    Parameters
+    ----------
+    image : np.ndarray
+        Image to process
+
+    resize : float, optional
+        Resize the image before performing any processing.
+
+    kernel : tuple, optional
+        Size of kernel to use when running the gaussian filter.
+
+    factor : int, float
+    	Factor to pass to the mean threshold.
+
+    Returns
+    -------
+    np.ndarray
+    	Array containing all the relevant fields of the image    
+    """
+    # Preprocess with a gaussian filter
+    image_prep = uint_resize_gauss(image, fx=resize, fy=resize, kernel=kernel,
+                                   mode=uint_mode)
     
-#     # Get images for all the shots
-#     data = yield from measure(detectors, num=num, delay=delay, filters=filters
-#                                 drop_missing=drop_missing)
+    # The main pipeline
+    try:
+        contour, area = get_largest_contour(image_prep, thesh_mode=thresh_mode,
+                                            factor=thresh_factor)
+        M = get_moments(contour=contour)
+        centroid_y, centroid_x = [pos//resize for pos in get_centroid(M)]
+        l, w = [val//resize for val in get_contour_size(contour=contour)]
+        match = get_similarity(contour)
+
+    # No beam on Image, set values to make this clear
+    except NoContoursDetected:
+        area = -1
+        centroid_y, centroid_x = [-1, -1]
+        l = -1
+        w = -1   
+        match = -1
+
+    mean_raw = image.mean()
+    mean_prep = image_prep.mean()
+    sum_raw = image.sum()
+    sum_prep = image_prep.sum()
     
-#     # Process the data    
-#     results = process_data(data, detectors, n_pool=Pool)
+    return np.array([mean_raw, mean_prep, sum_raw, sum_prep, area, centroid_x,
+                     centroid_y, l, w, match])
+
+def process_det_data(data, detectors, kernel=(13,13), uint_mode="scale",
+                     thresh_mode="otsu", thresh_factor=3, resize=1.0):
+    """
+    Processes each image in the dict and returns another dict with the
+    processed data.
+    """
+    result = dict()
+    image_data = {det.name : np.zeros((len(data), 10)) for det in detectors}
+    for det in detectors:
+        for i, d in enumerate(data):
+            # Array of processed image data for each shot in a dict for each det
+            image_data[det.name][i,:] = process_image(
+                d[det.name].reshape((256,256)),
+                kernel=kernel, resize=resize, uint_mode=uint_mode,
+                thresh_mode=thresh_mode)
+
+        # Turn the data into a mean and std for each entry
+        result_array = np.zeros((image_data[det.name].shape[1] * 2))
+        for i in range(image_data[det.name].shape[1]):
+            result_array[2*i] = image_data[det.name][:,i].mean()
+            result_array[2*i+1] = image_data[det.name][:,i].std()
+
+        # Key the array by det name
+        result[det.name] = result_array
+        
+    return result
+
+def characterize(detectors, image_signal, num=10, filters=None, delay=None,
+                 drop_missing=True, kernel=(9,9), resize=1.0, uint_mode="scale",
+                 min_area=100, factor=3, n_pool=None, **kwargs):
+    """
+    Returns a dictionary containing all the relevant statistics of the beam.
+    """
+    # Apply the default filter
+    if filters is None:
+        filters = {image_signal : lambda image : contour_area_filter(
+            to_image(image), kernel=kernel, resize=resize, uint_mode=uint_mode,
+            min_area=min_area, factor=factor, **kwargs)}
     
-#     # Set the read_attrs back
-#     for det in detectors:
-#         det.read_attrs = read_attr_orig[det.name]
-                                
-#     # return
-#     return results
+    # Get images for all the shots
+    data = yield from measure(getattr(detectors, image_signal), num=num,
+                              delay=delay, filters=filters,
+                              drop_missing=drop_missing)
+    
+    # Process the data    
+    results = process_data(data, detectors, image_signal, n_pool=Pool)
+                                    
+    # return
+    return results
 
 
 # class Processor(object):
@@ -230,4 +302,3 @@ logger = logging.getLogger(__name__)
 
 #     res = measure_beam_quality(p.detector, p.detector.image1, read_rate=100, num_reads=10)
 
-#     import IPython; IPython.embed()
