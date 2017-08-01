757c8707bcaea11e8ae621391393939c33191859
apra93 <dd.apra93@gmail.com>
2017-08-01 15:49:53 -0700

    ENH: Added real images from dg3 and hx2 as testing images.

---
+++ psbeam/images/testing/hx2/__init__.py
@@ -0,0 +1,36 @@
+"""
+Init file for images that can be used as templates or stock images in psbeam.
+
+To add a new directory, copy this init file into the new one, and change the
+relative import dir according to how deep it is in the directory tree.
+"""
+############
+# Standard #
+############
+import os
+
+###############
+# Third Party #
+###############
+import cv2
+
+##########
+# Module #
+##########
+from ....utils import get_images_from_dir   #Change this to match depth
+
+# Grab all the images in this directory
+images = get_images_from_dir(
+    os.path.dirname(os.path.abspath(__file__)), out_type=dict)
+
+# Make the images available in the namespace as nd.arrays and empty the dict
+for name, image in template_images.items():
+    exec(name + "=image")
+
+# Cleanup
+try:
+    del(name, image)
+except NameError:
+    # Happens if there is no image in the directory
+    pass
+
