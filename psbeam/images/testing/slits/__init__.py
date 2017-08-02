6a0154d8ccc2d6aeee3432d059d52eaab945c35e
apra93 <dd.apra93@gmail.com>
2017-08-02 12:28:50 -0700

    MAINT: Fixing image imports.

--- psbeam/images/testing/slits/__init__.py
+++ psbeam/images/testing/slits/__init__.py
@@ -24,7 +24,7 @@ images = get_images_from_dir(
     os.path.dirname(os.path.abspath(__file__)), out_type=dict)
 
 # Make the images available in the namespace as nd.arrays and empty the dict
-for name, image in template_images.items():
+for name, image in images.items():
     exec(name + "=image")
 
 # Cleanup
