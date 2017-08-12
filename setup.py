281f28955c40162d92df22df8f790a00cd1c8621
apra93 <dd.apra93@gmail.com>
2017-08-11 18:34:52 -0700

    BLD: Adding lines in manifest to include all files in psbeam/images.

--- setup.py
+++ setup.py
@@ -8,4 +8,5 @@ setup(name        = 'psbeam',
       license     = 'BSD',
       author      = 'SLAC National Accelerator Laboratory',
       packages    = find_packages(),
+      include_package_data = True      
       )
